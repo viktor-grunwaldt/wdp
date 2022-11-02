63b5001ea9dcbf274dffdb9efcf2a760ac83da23
Viktor Grünwaldt <vik777gru@gmail.com>
2022-11-02 18:19:46 +0100

    l4

--- lista03/zad4.py
+++ lista03/zad4.py
@@ -12,7 +12,7 @@ t.right(-90)
 t.pendown()
 
 # function to plot a Y
-def tree(sz, level, angle):
+def tree(sz, level, angle, rand):
     if level > 0:
         # drawing the base
         t.width(level)
@@ -22,18 +22,18 @@ def tree(sz, level, angle):
 
         # recursive call for
         # the right subtree
-        tree(0.8 * sz, level - 1, angle + random.randint(-10, 10))
+        tree(0.8 * sz, level - 1, angle + random.randint(-rand, rand), rand)
 
         t.left(2 * angle)
 
         # recursive call for
         # the left subtree
-        tree(0.8 * sz, level - 1, angle + random.randint(-10, 10))
+        tree(0.8 * sz, level - 1, angle + random.randint(-rand, rand), rand)
 
         t.right(angle)
         t.forward(-sz)
 
 
 # tree of size 80 and level 7
-tree(150, 8, 30)
+tree(150, 7, 30, 15)
 input()
